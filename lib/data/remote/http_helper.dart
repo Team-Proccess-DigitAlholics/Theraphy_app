9c46299d14ae0ebd651caea9224951205991e37c
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-09-01 13:41:47 -0500

    feat(htt_helper): crate HttHelper class

---
+++ lib/data/remote/http_helper.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
+
+import 'package:http/http.dart' as http;
+
+
+class HttpHelper {
+  final String urlBase =
+      'https://backendproyectotheraphy-production-5698.up.railway.app/api/v1';
+
+
+
+  
+}
