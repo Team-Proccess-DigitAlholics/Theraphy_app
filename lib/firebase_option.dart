165f65e2b7a3f6f578d27964232a213b5f753b96
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2023-09-01 22:41:19 -0500

    feat(treatment): added class for firebase services

---
+++ lib/firebase_option.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:theraphy_physiotherapist_app/firebase_options.dart';
+import 'package:theraphy_physiotherapist_app/ui/initial_views/login.dart';
+
+void main() async {
+
+  
+
+  //aea
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove("userId");
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Login());
+  }
+}
