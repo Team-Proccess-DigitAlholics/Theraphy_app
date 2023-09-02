aa5e45006d2079d3264123071c9f4c3bd8115afa
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2023-09-01 22:44:58 -0500

    feat(treatment): added widget of Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:theraphy_physiotherapist_app/firebase_options.dart';
+import 'package:theraphy_physiotherapist_app/ui/initial_views/login.dart';
+
+void main() async {
+
+  
+
+  //aea
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove("userId");
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Login());
   }
 }
