5362347b79cb301c12ab07b5eafe94b7eb973e2e
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-09-16 10:15:38 -0500

    feat(appointments): modified appoitnment entity

--- lib/data/model/appointment.dart
+++ lib/data/model/appointment.dart
@@ -3,8 +3,8 @@ import 'package:theraphy_physiotherapist_app/data/model/physiotherapist.dart';
 
 class Appointment {
   int id;
-  Patient patient;
-  Physiotherapist physiotherapist;
+  
+ 
   String scheduledDate;
   String topic;
   String done;
@@ -12,8 +12,7 @@ class Appointment {
 
   Appointment(
       {required this.id,
-      required this.patient,
-      required this.physiotherapist,
+     
       required this.scheduledDate,
       required this.topic,
       required this.done,
@@ -22,8 +21,7 @@ class Appointment {
   Appointment.fromJson(Map<String, dynamic> json)
       : this(
             id: json['id'],
-            patient: Patient.fromJson(json['patient']),
-            physiotherapist: Physiotherapist.fromJson(json['physiotherapist']),
+          
             scheduledDate: json['scheduledDate'],
             topic: json['topic'],
             done: json['done'],
